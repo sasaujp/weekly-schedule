72daa0b80dbe3e77b7875396560d2beb8fef3510
Fumiya Kubota <fumiya.kubota@glucose.jp>
2022-06-28 03:29:31 +0900

    update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 module.exports = {
+  assetPrefix: isProd ? '/weekly-schedule' : '',
   reactStrictMode: true,
 }
