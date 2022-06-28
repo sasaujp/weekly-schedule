200bac65ea9f4d816ba8b71371b936f49aa2a7e7
sasaujp <sasaujp@users.noreply.github.com>
2022-06-28 11:11:22 +0000

    deploy: 0b00090ce24bc02ef7b0271ad3fc2646a3e9d3a2

---
+++ _next/static/GaNFzXvE_r4Kewb4Fuxvn/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/947-31089fe4253af84c.js","static/chunks/pages/index-864e3c1b0415d415.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
