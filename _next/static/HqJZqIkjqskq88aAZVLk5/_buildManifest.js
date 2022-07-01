24206fac47d96f00bd2a304ce4001535558d74ca
sasaujp <sasaujp@users.noreply.github.com>
2022-07-01 19:47:52 +0000

    deploy: 5ae9328fcfc8f5ee3262970f29d83eb27ae90b50

--- _next/static/HqJZqIkjqskq88aAZVLk5/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/859-46f4914cf58163d1.js","static/chunks/pages/index-45a200ede6ab759a.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
