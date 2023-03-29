7135e104327018b5c048a70ad05780706caffe6d
sasaujp <sasaujp@users.noreply.github.com>
2023-03-29 23:27:24 +0000

    deploy: 1aa67e790f6466bd4d26dbfc23bd52ba9e2ae468

--- _next/static/aCyuUj_gVtuWriidAedwn/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/60-6e2e50edc9e36643.js","static/chunks/pages/index-27ca32df1e51e4d4.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/t8byHkJFOj3jZMW-xH2CJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/60-6e2e50edc9e36643.js","static/chunks/pages/index-0efac0fb802e8a89.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
